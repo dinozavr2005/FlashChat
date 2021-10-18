20d70eb08fbf4a0ab72e1117d2dac9e044f2e543
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2021-10-18 22:51:19 +0300

    full version

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Владимир on 18.10.2021.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
